cc2ce1d3f90c6ba8f50c9966355d58b712a22311
David May <49894298+wass3rw3rk@users.noreply.github.com>
2023-10-11 09:13:24 -0500

    chore(license): update source code headers + copyright year (#425)

--- cmd/secret-vault/main.go
+++ cmd/secret-vault/main.go
@@ -1,6 +1,4 @@
-// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
-//
-// Use of this source code is governed by the LICENSE file in this repository.
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -22,7 +20,7 @@ func main() {
 	app.Name = "secret-vault"
 	app.HelpName = "secret-vault"
 	app.Usage = "Vela Vault secret plugin for sourcing secrets into pipelines"
-	app.Copyright = "Copyright (c) 2022 Target Brands, Inc. All rights reserved."
+	app.Copyright = "Copyright 2020 Target Brands, Inc. All rights reserved."
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Vela Admins",
